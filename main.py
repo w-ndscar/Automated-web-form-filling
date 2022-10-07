8e06fb6c904a9c6f3e7d1014fac9f4b48a35145e
bathri77 <narayanabathri777@gmail.com>
2022-10-07 21:27:38 +0530

    code

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 from selenium import webdriver
 import xlrd
  
+driver = webdriver.Chrome( executable_path="")
+driver.get("")
+
+name = []
+short_form = []
+e_Address = []
+passwd = []
+
+file_location = ""
+workbook = xlrd.open_workbook(file_location)
+sheet = workbook.sheet_by_index(0)
+for col in range(sheet.ncol):
+    name.append(sheet.cell_value(col,0))
+    short_form.append(sheet.cell_value(col,1))
+    e_Address.append(sheet.cell_value (col,2))
+    passwd.append(sheet.cell_value(col,3))
+
+print (name)
+print (short_form)
+print (e_Address)
+print (passwd)
+
+  
