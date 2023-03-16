3c0593fb6275f9cfa1e51c7289b794be7c0332a3
windskar <arun070996@gmail.com>
2023-03-16 16:32:18 +0530

    Working Code

--- Add_drawings.py
+++ Add_drawings.py
@@ -1,81 +1,125 @@
+#!/usr/bin/env python
+
 from time import sleep
 from turtle import clear
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+#from webdriver_manager.chrome import ChromeDriverManager
 import keyboard
 import openpyxl
 from openpyxl.utils import get_column_letter
 
 #Chrome driver
-chrome_driver = "C:/chromedriver.exe"
+ser = Service(r"C:/chromedriver.exe")
 
 #Chrome - Debugger Mode
-chrome_options = Options()
-chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-driver = webdriver.Chrome(chrome_driver, chrome_options=chrome_options)
+options = Options()
+options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+driver = webdriver.Chrome(service=ser, options=options)
 
 #Just for a reference - see console
 print(driver.title)
 
 #Lists to store the Excel values
-proj = []
+element = []
+sh_size = []
+st_date = []
 dwg_name = []
 dwg_desc = []
 
+#User Input
+she_name = input("Please enter the Excel Sheet Name: ")
+no_of_rows = input("Please enter the ending row number + 1: ")
+no_of_rows = int(no_of_rows)
+
+
+print(no_of_rows)
 
 #Load workbook and sheet
-file_location = "C:/Users/Arun/Documents/Timesheet/Timesheet_Team_Copy.xlsx"
-workbook = openpyxl.load_workbook(file_location)
-sheet = workbook["Team7"]
+file_location = "C:/Users/Arun/Documents/Timesheet/ESMT_ENTRY.xlsx"
+workbook = openpyxl.load_workbook(file_location, data_only=True)
+sheet = workbook[she_name]
 
-#Loops to store values in the lists
-col = 2
-for row in range(3, 11):
+#Loops to store Excel values in the lists
+col = 1
+for row in range(3, no_of_rows):
     char = get_column_letter(col)
     cell_name = char + str(row)
-    proj.append(sheet[cell_name].value)
-    
+    element.append(sheet[cell_name].value)
+
 col=col+1
-for row in range(3, 11):
+for row in range(3, no_of_rows):
+    char = get_column_letter(col)
+    cell_name = char + str(row)
+    sh_size.append(sheet[cell_name].value)
+
+col=col+1
+for row in range(3, no_of_rows):
+    char = get_column_letter(col)
+    cell_name = char + str(row)
+    dt = sheet[cell_name].value
+    c_dt = dt.strftime('%d-%m-%Y')
+    st_date.append(c_dt)
+
+col=col+1
+for row in range(3, no_of_rows):
     char = get_column_letter(col)
     cell_name = char + str(row)
     dwg_name.append(sheet[cell_name].value)
 
 col=col+1
-for row in range(3, 11):
+for row in range(3, no_of_rows):
     char = get_column_letter(col)
     cell_name = char + str(row)
     dwg_desc.append(sheet[cell_name].value)
 
 
 #Printing just for a reference
-print (proj)
+print (element)
+print (sh_size)
+print (st_date)
 print (dwg_name)
 print (dwg_desc)
 
 
 sleep(1)
 
-
-for i in range(len(proj)):
+#Clicks and Enters the lists' values into the form in a loop
+for i in range(len(element)):
     #Click - New button
     elem2 = driver.find_element("xpath", '/html/body/app-root/app-project-detail/div/div[4]/div/button')
     elem2.click()
     sleep(1)
+    
     #Fields
+
+    #Element
     elem3 = driver.find_element("xpath", '/html/body/app-root/app-project-detail/div/div[4]/div/div/div/div/div[1]/ng-autocomplete/div[1]/div[1]/input')
-    elem3.send_keys(proj[i])
-    elem4 = driver.find_element("xpath", '/html/body/app-root/app-project-detail/div/div[4]/div/div/div/div/input[1]')
-    elem4.send_keys(dwg_name[i])
-    elem5 = driver.find_element("xpath", '/html/body/app-root/app-project-detail/div/div[4]/div/div/div/div/input[2]')
-    elem5.send_keys(dwg_desc[i])
+    elem3.send_keys(element[i])
+    #Sheet Size - Dropdown
+    sleep(1)
+    elem5 = Select(driver.find_element("xpath", '/html/body/app-root/app-project-detail/div/div[4]/div/div/div/div/select')).select_by_visible_text(sh_size[i])
+    #elem5.send_keys(sh_size[i])
+    sleep(0.5)
+    #Start Date
+    elem5 = driver.find_element("xpath", '/html/body/app-root/app-project-detail/div/div[4]/div/div/div/div/input[1]')
+    elem5.send_keys(st_date[i])
+    sleep(1)
+    #Drawing name/number
+    elem6 = driver.find_element("xpath", '/html/body/app-root/app-project-detail/div/div[4]/div/div/div/div/input[2]')
+    elem6.send_keys(dwg_name[i])
+    #Drawing Desc.
+    elem7 = driver.find_element("xpath", '/html/body/app-root/app-project-detail/div/div[4]/div/div/div/div/input[3]')
+    elem7.send_keys(dwg_desc[i])
     
     #Hotkey to wait before saving
     keyboard.wait("esc")
 
-    #Dropdown
-    elem6 = driver.find_element("xpath", '/html/body/app-root/app-project-detail/div/div[4]/div/div/div/div/div[2]/button[1]')
-    elem6.click
+    #Click - Save button
+    elem8 = driver.find_element("xpath", '/html/body/app-root/app-project-detail/div/div[4]/div/div/div/div/div[2]/button[1]')
+    elem8.click
     sleep(0.5)
     
     #Hotkey to wait before the next iteration
